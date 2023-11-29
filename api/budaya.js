8e48902c529623f4b9cd10759426737e1baf6bb5
RiqqiAmru <Riqqi0069@stmik-wp.ac.id>
2023-11-29 13:43:47 +0700

    add data budaya aceh

---
+++ api/budaya.js
@@ -0,0 +1,96 @@
+const budaya = [
+  {
+    id: 1,
+    provinsi: 'Aceh',
+    makanan: [
+      {
+        nama: 'Mie Aceh',
+        gambar: 'Mie-Aceh-1.jpg',
+      },
+      {
+        nama: 'Kuah Pliek U',
+        gambar: 'Kuah-Pliek-U.jpg',
+      },
+      {
+        nama: 'Gulai Kambing Aceh',
+        gambar: 'Gulai-Kambing-Aceh.jpg',
+      },
+    ],
+    tarian: [
+      {
+        nama: 'Tarek Pukat Aceh',
+        gambar: 'tari-pukat-aceh.webp',
+        description:
+          'menggambarkan aktivitas nelayan yang akan melaut untuk menangkap ikan. Menurut sejarah, tarian ini memang terinspirasi dari tradisi para nelayan Aceh yang memang sebagian besar profesinya adalah nelayan. Makna singkat dari tarian ini adalah kerja sama serta kebersamaan yang diiringi dengan alat musik tradisional',
+      },
+      {
+        nama: 'Seudati',
+        gambar: 'seudati.webp',
+        description:
+          'Tarian ini merupakan tarian yang sangat populer di Aceh. Tarian ini biasanya ditampilkan pada acara-acara besar seperti pernikahan, penyambutan tamu, dan lain-lain. Tarian ini biasanya ditampilkan oleh sekelompok penari yang terdiri dari laki-laki dan perempuan. Tarian ini biasanya diiringi dengan alat musik tradisional seperti gendang, rebana, dan lain-lain.',
+      },
+      {
+        nama: 'Bines',
+        gambar: 'bines.webp',
+        description:
+          'Tarian daerah Aceh bernama tari bines berasal dari Kabupaten Gayo Lues yang umumnya akan dipertunjukan oleh sekelompok wanita. Untuk jumlah penarinya sendiri harus berjumlah genap dari 10, 12, dan angka genap lainnya. Ciri khas dari tarian Aceh ini adalah tarian yang dilakukan dengan gerakan lambat dan berkembang menjadi cepat sampai akhirnya berhenti secara serentak yang hampir serupa dengan tari saman. Selain itu, para penonton yang ingin memberikan uang juga harus menyimpan uang tersebut di atas kepala penari. Uang tersebut dianggap sebagai pengganti bunga yang sudah diberikan penari ketika akhir acara. Sedangkan kostum yang dipakai dalam tarian ini terdiri dari baju lukup, kain sarung yang seragam, kain panjang, hiasan di bagian leher dan juga hiasan tangan seperti topong gelang dengan diiringi lagu Jangin Bines.',
+      },
+    ],
+    upacara: [
+      {
+        nama: 'Peusijuek',
+        gambar: 'Peusijuek.jpg',
+        description:
+          'Peusijuek ini biasanya dipimpin oleh seorang tokoh agama atau tokoh adat yang dituakan oleh masyarakat. Dalam prosesinya, tradisi ini diisi dengan doa keselamatan dan kesejahteraan bersama sesuai dengan ajaran agama Islam. Upacara adat ini dilakukan oleh masyarakat Aceh sebagai ungkapan rasa syukur atas keselamatan dan kesuksesan dalam meraih sesuatu.',
+      },
+      {
+        nama: 'Meugang',
+        gambar: 'Meugang.jpg',
+        description:
+          'Meugang merupakan tradisi masyarakat Aceh yang dilakukan pada bulan Ramadhan. Tradisi ini dilakukan dengan cara mengunjungi rumah sanak saudara dan tetangga untuk bersilaturahmi. Selain itu, tradisi ini juga dilakukan dengan cara membagikan makanan kepada tetangga dan sanak saudara. Makanan yang biasanya dibagikan adalah makanan yang terbuat dari daging seperti rendang, gulai kambing, dan lain-lain.',
+      },
+      {
+        nama: 'Kenduri Beureuat',
+        gambar: 'Kenduri-Beureuat.jpg',
+        description:
+          'Meugang atau yang juga dikenal sebagai Makmeugang ini merupakan salah satu upacara adat di Aceh yang masih dilakukan sampai saat ini. Meugang ini meruoakan tradisi menyembelih hewan kurban berupa kambing atau sapi yang dilaksanakan setiap tiga kali setahun, yaitu saat Ramadhan, Idul Fotri dan Idul Adha.Daging dari penyembelihan kurban tersebut kemudian diolah oleh masyarakat Aceh dan dinikmati bersama keluarga, keraban serta dibagikan pada yatim piatu. Tradisi ini biasanya dilaksanakan sehari sebelum bulan Ramadhan atau Hari Raya Idul Fitri. Meugang ini konon telah dilakukan sejak ratusan tahun lalu pada masa Kerajaan Aceh yang mana tujuannya sebagai wujud rasa syukur atas kemakmuran rakyatnya serta rasa terima kasih kepada seluruh rakyatnya.',
+      },
+    ],
+    pakaian: [
+      {
+        nama: 'Linta Baro',
+        gambar: 'linta-baro.jpg',
+        description:
+          'Baju adat Linta Baro atau Peukayan Linta Baro adalah pakaian yang khusus dikenakan oleh para pria.Pada jaman dulu, biasanya busana ini dipakai untuk kepentingan acara kerajaan dan untuk upacara adat dari kerajaan besar di Aceh, yaitu Kerajaan Perlak dan Kesultanan Samudra Pasai.Pakaian adat Linta Baro dibagi menjad 3 bagian, yakni bagian atas, bagian tengah, dan bagian bawah, lengkap dengan senjata tradisional nya.',
+      },
+      {
+        nama: 'Meukasah',
+        gambar: 'meukasah.jpg',
+        description:
+          'Meukasah adalah salah satu bagian penting dari baju adat Linta Baro.Baju atasan dengan lengan panjang ini ditenun dari bahan benang sutra.Biasanya, warna yang digunakan untuk Meukasah ini adalah warna hitam, sebab masyarakat setempat meyakini warna hitam adalah simbol kebesaran.Pada bagian kerah Meukasah dibuat tertutup, serta dilengkapi dengan sulaman yang dijahit memakai benang emas sampai ke depan dada.Walaupun budaya Islam dan Melayu cukup dominan dan kental dalam pakaian ini, sentuhan budaya Cina ternyata juga sedikit melekat di dalamnya.Hal ini tidak lepas dari sejarah Aceh, yang dulunya masuk dalam jalur perdagangan Tiongkok.',
+      },
+    ],
+    rumah: {
+      nama: 'Rumah Adat Aceh',
+      gambar: 'rumah-adat-aceh.jpg',
+      description:
+        'Rumah Adat Aceh – Tak ayal, rumah adat Aceh juga sarat akan makna dan filosofi keislaman yang sangat religius. Negeri Serambi Mekkah adalah julukan untuk Provinsi Nanggroe Aceh Darussalam. Provinsi paling barat di Indonesia ini, memang merupakan Provinsi pertama tempat penyebaran agama Islam. Sehingga budaya Islam sangat kental terasa di provinsi ini.Provinsi Aceh juga memiliki rumah adat yang menjadi simbol budayanya seperti daerah-daerah lain. Setiap adat menyimpan keunikan-keunikan tersendiri. Tidak ada pengecualian termasuk rumah adat di Aceh.Rumah adat Aceh berbentuk rumah panggung yang memiliki tinggi tiang 2,5 hingga 3 meter. Bentuk panggung dipilih agar penghuni rumah terhindar dari serangan binatang buas. Rumah panggung juga dibangun agar air tidak masuk ke dalam rumah apabila terjadi banjir.Hal yang tidak biasa dari rumah adat Aceh adalah pintu yang memiliki tinggi 120 hingga 150 cm. Sehingga, terkadang masyarakat harus menunduk agar dapat masuk ke dalam rumah tersebut. Lalu, bagaimana sejarah, bagian-bagian, dan filosofi yang ada pada rumah adat Aceh',
+    },
+    senjata: [
+      {
+        nama: 'Rencong Meupucok',
+        gambar: 'rencong-meupocok.jpg',
+        description:
+          'Meupecok adalah jenis rencong yang masuk senjata golongan atas bagi rakyat Aceh.Hal ini merujuk pada fungsinya yang dahulu hanya diperuntukkan sebagai senjata para sultan, sedangkan sekarang lebih difungsikan untuk mendukung kegiatan adat.Keunikan senjata tradisional ini terletak pada pangkal bilah di atas gagang yang terdapat pucuk dari bahan emas murni.',
+      },
+      {
+        nama: 'Rencong Meucugek',
+        gambar: 'rencong-meucugek.jpg',
+        description:
+          'Penamaan rencong meucugek berdasarkan pada komposisi senjatanya yang memiliki cugek, yaitu suatu perekat sekaligus penahan pada gagang pegangan.Gagang ini melengkung ke arah mata rencong hingga 90º atau sekitar 8-10 cm.Cugek ini memberikan kekuatan pegangan saat rencong digunakan sehingga tidak mudah lepas dari tangan.Penggunaan rencong meucugek sangat terkenal sebagai alat tikam andalan untuk melawan penjajahan Belanda.',
+      },
+    ],
+  },
+];
+
+export default budaya;
--- readme.md
+++ readme.md
@@ -18,5 +18,4 @@ data Budaya
 - Rumah Adat
 - Senjata Tradisional
 - Tarian Tradisional
-- Lagu Daerah
 - Makanan Khas
