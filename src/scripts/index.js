6c3b9107fa1974e4c5e5a233f2320231f1ac4c7d
ikkromm18 <muhammadikkrom749@gmail.com>
2023-12-07 03:08:17 +0700

    perubahan kecil

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,12 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
+// eslint-disable-next-line no-unused-vars
+import Swiper from 'swiper';
+// eslint-disable-next-line import/no-unresolved
 import App from './views/app';
 
+console.log(Swiper);
 // eslint-disable-next-line no-unused-vars
 const app = new App({
   button: document.querySelector('.drawer'),
